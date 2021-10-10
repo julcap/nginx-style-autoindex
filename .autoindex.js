12f1ce5aa9fafc01f8bed191dd02a89f6cf0842a
Julian Capilla <jca@bigtincan.com>
2021-10-10 17:49:03 +0200

    Add back arrow

--- .autoindex.js
+++ .autoindex.js
@@ -18,7 +18,7 @@
         parseSize: function (strSize) {
             if (!strSize) return null;
             let calculate = (size, index = 0) => {
-                if (size.length < 5) return [size, index]
+                if (size < 900) return [size, index]
                 return calculate(parseFloat(size / 1024).toFixed(2), ++index);
             }
             let metrics = ['bytes', 'Kb', 'Mb', 'Gb', 'Tb'];
@@ -46,11 +46,12 @@
                 }
 
             })
-            if (html.length) {
-                html.unshift('<ul class="nginx-list">');
-                html.push('</ul>');
-                nginxList.innerHTML = html.join('');
-            }
+            html[0] = `<li class="back"><a href="../"><svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26.676 26.676" xml:space="preserve">
+                        <path d="M26.105 21.891a.568.568 0 0 1-.529-.346c-.066-.156-1.716-3.857-7.885-4.59-1.285-.156-2.824-.236-4.693-.25v4.613a.574.574 0 0 1-.304.508.577.577 0 0 1-.588-.033L.254 13.815a.573.573 0 0 1 0-.953l11.857-7.979a.563.563 0 0 1 .588-.029c.19.102.303.295.303.502v4.293c2.578.336 13.674 2.33 13.674 11.674a.574.574 0 0 1-.459.562c-.037.006-.076.006-.112.006z"/>
+                        </svg></a></li>`
+            html.unshift('<ul class="nginx-list">');
+            html.push('</ul>');
+            nginxList.innerHTML = html.join('');
         }
     };
     nginx.init();
